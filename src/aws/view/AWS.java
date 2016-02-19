86c6902e4b50d2d5d4200004cc2bb2a0bca36ce4
Lukas Normantas <l.normantas@se14.qmul.ac.uk>
2016-02-19 12:06:07 +0000

    Initial commit

---
+++ src/aws/view/AWS.java
@@ -0,0 +1,44 @@
+package aws.view;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Lukas
+ */
+public class AWS extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) {
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction((ActionEvent event) -> {
+            System.out.println("Hello World!");
+        });
+        
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        
+        Circle circle = new Circle();
+        circle.setCenterX(100.0f);
+        circle.setCenterY(100.0f);
+        circle.setRadius(50.0f);
+        
+        Scene scene = new Scene(root, 300, 250);
+        
+        primaryStage.setTitle("Hello World!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
