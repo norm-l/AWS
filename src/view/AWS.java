8293d5bab24d740f91b613f8e70842121a359b48
Lukas Normantas <l.normantas@se14.qmul.ac.uk>
2016-02-20 16:47:18 +0000

    Panes/scene switching

--- src/view/AWS.java
+++ src/view/AWS.java
@@ -4,7 +4,7 @@ import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
@@ -12,24 +12,33 @@ import javafx.stage.Stage;
 
 public class AWS extends Application {
     
+    Stage thestage;
+    Scene scene1, scene2;
+    
     @Override
     public void start(Stage primaryStage) {
-        //Button btn = new Button();
-        //btn.setText("Say 'Hello World'");
-        //btn.setOnAction((ActionEvent event) -> {
-        //    System.out.println("Hello World!");
-        //});
-        
-        StackPane root = new StackPane();
-        //root.getChildren().add(btn);
-        
+        Button btn = new Button();
+        btn.setText("Tablet Version");
         Circle circle = new Circle(100, Color.BLACK);
-        root.getChildren().add(circle);
         
-        Scene scene = new Scene(root, 320, 480); // Small [Phone]: 320x480 | Large [Tablet]: 1024x768
+        thestage = primaryStage;
+        BorderPane pane1 = new BorderPane();
+        BorderPane pane2 = new BorderPane();
+        
+        pane1.getChildren().add(btn);
+        pane1.setCenter(btn);
+        
+        scene1 = new Scene(pane1, 320, 480);
+        scene2 = new Scene(pane2, 1024, 768);
+        
+        // Small [Phone]: 320x480 | Large [Tablet]: 1024x768
+        
+        btn.setOnAction((ActionEvent event) -> {
+            thestage.setScene(scene2);
+        });
 
         primaryStage.setTitle("AWS");
-        primaryStage.setScene(scene);
+        primaryStage.setScene(scene1);
         primaryStage.show();
     }
 
