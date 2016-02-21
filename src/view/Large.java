f047d17bacca0c7665af49aa6f504d6ef762c0ef
Lukas Normantas <l.normantas@se14.qmul.ac.uk>
2016-02-21 19:18:30 +0000

    Attempt in switching scenes/stages

---
+++ src/view/Large.java
@@ -0,0 +1,18 @@
+package view;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+
+public class Large extends Application {
+    
+    Stage stage;
+
+    @Override
+    public void start(Stage stage) {
+    }
+    
+    public void setStage(Stage stage) {
+        this.stage = stage;
+    }
+}
