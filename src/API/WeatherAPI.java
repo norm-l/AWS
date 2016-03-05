f96cfb909a55f38a14dd4564269136af8a83d6f5
Lukas Normantas <l.normantas@se14.qmul.ac.uk>
2016-03-05 19:55:05 +0000

    WeatherAPI implementation

--- src/API/WeatherAPI.java
+++ src/API/WeatherAPI.java
@@ -2,15 +2,21 @@ package API;
 
 import com.github.dvdme.ForecastIOLib.FIOHourly;
 import com.github.dvdme.ForecastIOLib.ForecastIO;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TimeZone;
 
 public class WeatherAPI {
 
     // API KEY: 342c4be47e8f894c2e0f514f31d93dae
     ForecastIO fio;
+    Map hourlyData = new HashMap();
 
     public WeatherAPI(String APIKey) {
         fio = new ForecastIO(APIKey);
-        fio.setUnits(ForecastIO.UNITS_SI);
+        fio.setUnits(ForecastIO.UNITS_UK);
         fio.setLang(ForecastIO.LANG_ENGLISH);
         fio.getForecast("51.5072", "0.1275");
         System.out.println("Latitude: " + fio.getLatitude());
@@ -18,23 +24,18 @@ public class WeatherAPI {
         System.out.println("Timezone: " + fio.getTimezone());
     }
 
-    public void HourlyReport() {
+    public Map HourlyReport(int hour) {
         FIOHourly hourly = new FIOHourly(fio);
         //In case there is no hourly data available
         if (hourly.hours() < 0) {
             System.out.println("No hourly data.");
         } else {
-            System.out.println("\nHourly:\n");
+            hourlyData.put("Temp", hourly.getHour(hour).temperature()); // Temperature
+            hourlyData.put("TempFeel", hourly.getHour(hour).apparentTemperature()); // Feel Temperature
+            hourlyData.put("Humidity", hourly.getHour(hour).humidity()); // Humidity
+            hourlyData.put("WindSpeed", hourly.getHour(hour).windSpeed()); // Wind Speed
+            hourlyData.put("TimeStamp", hourly.getHour(hour).time());// TimeStamp
         }
-        //Print hourly data
-        for (int i = 0; i < hourly.hours(); i++) {
-            String[] h = hourly.getHour(i).getFieldsArray();
-            System.out.println("Hour #" + (i + 1));
-            for (String h1 : h) {
-                System.out.println(h1 + ": " + hourly.getHour(i).getByKey(h1));
-            }
-            System.out.println("\n");
-        }
+        return hourlyData;
     }
-
 }
--- src/controllers/MainController.java
+++ src/controllers/MainController.java
@@ -1,10 +1,10 @@
 package controllers;
 
 import API.WeatherAPI;
-import com.github.dvdme.ForecastIOLib.ForecastIO;
+
 import java.net.URL;
+import java.util.Map;
 import java.util.ResourceBundle;
-import javafx.beans.binding.Bindings;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
@@ -16,55 +16,74 @@ public class MainController implements Initializable {
     private Label labelTest;
     @FXML
     private Slider timeSlider;
+    @FXML
+    private Label hourlyTemp;
+    @FXML
+    private Label hourlyFeelTemp;
+    @FXML
+    private Label hourlyHumidity;
+    @FXML
+    private Label hourlyWindSpeed;
+
     private int currentTime;
     private WeatherAPI weatherAPI;
+    private Map hourlyData;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+
+        weatherAPI = new WeatherAPI("342c4be47e8f894c2e0f514f31d93dae");
 
         timeSlider.valueProperty().addListener((obs, oldValue, newValue) -> {
             if (timeSlider.isValueChanging()) {
                 currentTime = newValue.intValue(); // Set the current time
                 updateTime();
-                if (newValue.intValue() < 12) {
-                    labelTest.textProperty().bind(Bindings.format("%.0f:00am", timeSlider.valueProperty())); // Adjust binding
-                } else {
-                    labelTest.textProperty().bind(Bindings.format("%.0f:00pm", timeSlider.valueProperty())); // Adjust binding
-                }
             }
         });
-        
+
         //timeSlider.valueChangingProperty().addListener((obs, wasChanging, isChanging) -> {
         //    if (!isChanging) {
         //        System.out.println(currentTime);
         //    }
         //});
-
-        weatherAPI = new WeatherAPI("342c4be47e8f894c2e0f514f31d93dae");
-        weatherAPI.HourlyReport();
+        //weatherAPI.HourlyReport(1);
     }
 
     private void updateTime() {
+
         switch (currentTime) {
             case 1:
-                System.out.println("1");
+                hourlyData = weatherAPI.HourlyReport(1);
+                setHourlyData();
                 break;
             case 2:
-                System.out.println("2");
+                weatherAPI.HourlyReport(2);
+                setHourlyData();
                 break;
             case 3:
-                System.out.println("3");
+                weatherAPI.HourlyReport(3);
+                setHourlyData();
                 break;
             case 4:
-                System.out.println("4");
+                weatherAPI.HourlyReport(4);
+                setHourlyData();
                 break;
             case 5:
-                System.out.println("5");
+                weatherAPI.HourlyReport(5);
+                setHourlyData();
                 break;
             default:
                 System.out.println("Don't know");
                 break;
         }
+    }
+
+    private void setHourlyData() {
+        hourlyTemp.setText("Temperature: " + hourlyData.get("Temp").toString() + "c");
+        hourlyFeelTemp.setText("Feels Like: " + hourlyData.get("TempFeel").toString() + "c");
+        hourlyHumidity.setText("Humidity: " + hourlyData.get("Humidity").toString());
+        hourlyWindSpeed.setText("Wind Speed: " + hourlyData.get("WindSpeed").toString());
+        labelTest.setText(hourlyData.get("TimeStamp").toString());
     }
 
 }
