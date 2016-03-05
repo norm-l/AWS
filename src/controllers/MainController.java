6582088e1ce3660577b0936399419e8f4287d671
Lukas Normantas <l.normantas@se14.qmul.ac.uk>
2016-03-05 15:47:25 +0000

    Slider working & functional

--- src/controllers/MainController.java
+++ src/controllers/MainController.java
@@ -16,32 +16,21 @@ public class MainController implements Initializable {
     private Slider timeSlider;
 
     private final int HOURS = 23;
-    private final int MIN_CHANGE = 1;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
 
         timeSlider.setMax(HOURS);
 
-        //labelTest.textProperty().bind(Bindings.format("%.0f:00am", timeSlider.valueProperty()));
-
-    timeSlider.valueChangingProperty().addListener((obs, wasChanging, isChanging) -> {
-        if (isChanging) { // If the slider has stopped changing set the new time
-            if(timeSlider.getValue() < 12.99) {
-                labelTest.textProperty().bind(Bindings.format("%.0f:00am", timeSlider.valueProperty()));
-            } else {
-                labelTest.textProperty().bind(Bindings.format("%.0f:00pm", timeSlider.valueProperty()));
+        timeSlider.valueProperty().addListener((obs, oldValue, newValue) -> {
+            if (timeSlider.isValueChanging()) {
+                if (newValue.intValue() < 12) {
+                    labelTest.textProperty().bind(Bindings.format("%.0f:00am", timeSlider.valueProperty()));
+                } else {
+                    labelTest.textProperty().bind(Bindings.format("%.0f:00pm", timeSlider.valueProperty()));
+                }
             }
-        }
-    });
-
-    /*timeSlider.valueProperty().addListener((obs, oldValue, newValue) -> {
-        if (!timeSlider.isValueChanging()) {
-            if (Math.abs(newValue.intValue()) > MIN_CHANGE) {
-                timeSlider.setValue(newValue.intValue());
-            }
-        }
-    });*/
+        });
     }
 
 }
