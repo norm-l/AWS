9e29ddde5455984af4157637aafdb9480f028546
Lukas Normantas <l.normantas@se14.qmul.ac.uk>
2016-03-06 02:46:20 +0000

    Weather changing + weatherAPI fixes + summary addition

--- src/API/WeatherAPI.java
+++ src/API/WeatherAPI.java
@@ -1,7 +1,10 @@
 package API;
 
+import com.github.dvdme.ForecastIOLib.FIOAlerts;
+import com.github.dvdme.ForecastIOLib.FIODaily;
 import com.github.dvdme.ForecastIOLib.FIOHourly;
 import com.github.dvdme.ForecastIOLib.ForecastIO;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -10,18 +13,17 @@ public class WeatherAPI {
     // API KEY: 342c4be47e8f894c2e0f514f31d93dae
     ForecastIO fio;
     Map hourlyData = new HashMap();
+    Map dailyData = new HashMap();
 
     public WeatherAPI(String APIKey) {
         fio = new ForecastIO(APIKey);
         fio.setUnits(ForecastIO.UNITS_UK);
         fio.setLang(ForecastIO.LANG_ENGLISH);
         fio.getForecast("51.5072", "0.1275");
-        System.out.println("Latitude: " + fio.getLatitude());
-        System.out.println("Longitude: " + fio.getLongitude());
         System.out.println("Timezone: " + fio.getTimezone());
     }
 
-    public Map HourlyReport(int hour) {
+    public Map hourlyReport(int hour) {
         FIOHourly hourly = new FIOHourly(fio);
         //In case there is no hourly data available
         if (hourly.hours() < 0) {
@@ -33,7 +35,28 @@ public class WeatherAPI {
             hourlyData.put("WindSpeed", hourly.getHour(hour).windSpeed()); // Wind Speed
             hourlyData.put("Precip", hourly.getHour(hour).precipProbability()); // Precipitation
             hourlyData.put("TimeStamp", hourly.getHour(hour).time());// TimeStamp
+            hourlyData.put("Summary", hourly.getHour(hour).summary()); // Summary
+            hourlyData.put("Icon", hourly.getHour(hour).icon());
         }
         return hourlyData;
     }
+
+    public Map dailyReport(int day) {
+        FIODaily daily = new FIODaily(fio);
+        //In case there is no daily data available
+        if (daily.days() < 0) {
+            System.out.println("No daily data.");
+        } else {
+            System.out.println("Data for day " + day);
+            Double temp1 = daily.getDay(day).temperatureMax();
+            System.out.println(temp1);
+            Double temp2 = daily.getDay(day).temperatureMin();
+            System.out.println(temp2);
+            Double result = temp1 + temp2;
+            dailyData.put("Temp", result); // Temperature
+            dailyData.put("Icon", daily.getDay(day).icon()); // Icon
+        }
+        return dailyData;
+    }
+
 }
--- src/controllers/MainController.java
+++ src/controllers/MainController.java
@@ -3,17 +3,20 @@ package controllers;
 import API.WeatherAPI;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.Map;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class MainController implements Initializable {
 
     @FXML
-    private Label labelTest;
+    private Label timeCheck;
     @FXML
     private Slider timeSlider;
     @FXML
@@ -27,46 +30,160 @@ public class MainController implements Initializable {
     @FXML
     private Label hourlyPrecip;
     
+    @FXML
+    private Label artInt;
+    
+    @FXML private Label day1;
+    @FXML private Label day1Temp;
+    @FXML private ImageView day1Img;
+    
+    @FXML private Label day2;
+    @FXML private Label day2Temp;
+    @FXML private ImageView day2Img;
+    
+    @FXML private Label day3;
+    @FXML private Label day3Temp;
+    @FXML private ImageView day3Img;
+    
+    @FXML private Label day4;
+    @FXML private Label day4Temp;
+    @FXML private ImageView day4Img;
+    
+    @FXML private Label day5;
+    @FXML private Label day5Temp;
+    @FXML private ImageView day5Img;
+    
+    @FXML private Label day6;
+    @FXML private Label day6Temp;
+    @FXML private ImageView day6Img;
+    
+    @FXML private ImageView iconImg;
+    
+    private final Image CLEAR_DAY = new Image(
+            getClass().getClassLoader().getResourceAsStream("resources/images/icons/clear_day.png"));
+    private final Image CLEAR_NIGHT = new Image(
+            getClass().getClassLoader().getResourceAsStream("resources/images/icons/clear_night.png"));
+    private final Image RAIN = new Image(
+            getClass().getClassLoader().getResourceAsStream("resources/images/icons/rain.png"));
+    private final Image SNOW = new Image(
+            getClass().getClassLoader().getResourceAsStream("resources/images/icons/snow.png"));
+    private final Image SLEET = new Image(
+            getClass().getClassLoader().getResourceAsStream("resources/images/icons/sleet.png"));
+    private final Image WIND = new Image(
+            getClass().getClassLoader().getResourceAsStream("resources/images/icons/wind.png"));
+    private final Image FOG = new Image(
+            getClass().getClassLoader().getResourceAsStream("resources/images/icons/fog.png"));
+    private final Image CLOUDY = new Image(
+            getClass().getClassLoader().getResourceAsStream("resources/images/icons/cloudy.png"));
+    private final Image PARTLY_CLOUDY_DAY = new Image(
+            getClass().getClassLoader().getResourceAsStream("resources/images/icons/partly_cloudy_day.png"));
+    private final Image PARTLY_CLOUDY_NIGHT = new Image(
+            getClass().getClassLoader().getResourceAsStream("resources/images/icons/partly_cloudy_night.png"));
+    private final Image UNKNOWN = new Image(
+            getClass().getClassLoader().getResourceAsStream("resources/images/icons/na.png"));
+    
     private int currentTime;
     private WeatherAPI weatherAPI;
     private Map hourlyData;
+    private Map dailyData;
+    private ArrayList<String> alertData;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
 
         weatherAPI = new WeatherAPI("342c4be47e8f894c2e0f514f31d93dae");
-
+        setDailyData();
+        
         timeSlider.valueProperty().addListener((obs, oldValue, newValue) -> {
             if (timeSlider.isValueChanging()) {
                 currentTime = newValue.intValue(); // Set the current time
                 updateTime();
             }
         });
-
-        //timeSlider.valueChangingProperty().addListener((obs, wasChanging, isChanging) -> {
-        //    if (!isChanging) {
-        //        System.out.println(currentTime);
-        //    }
-        //});
-        //weatherAPI.HourlyReport(1);
     }
 
     private void updateTime() {
         if(currentTime > 0 && currentTime < 25) {
-            hourlyData = weatherAPI.HourlyReport(currentTime);
+            hourlyData = weatherAPI.hourlyReport(currentTime);
             setHourlyData();
         } else {
-            System.out.println("Oops");
+            System.out.println("Error!");
         }
     }
 
     private void setHourlyData() {
-        hourlyTemp.setText("Temperature: " + hourlyData.get("Temp").toString() + "c");
-        hourlyFeelTemp.setText("Feels Like: " + hourlyData.get("TempFeel").toString() + "c");
-        hourlyHumidity.setText("Humidity: " + hourlyData.get("Humidity").toString());
-        hourlyWindSpeed.setText("Wind Speed: " + hourlyData.get("WindSpeed").toString());
-        hourlyPrecip.setText("Precip: " + hourlyData.get("Precip").toString());
-        labelTest.setText(hourlyData.get("TimeStamp").toString());
+        hourlyTemp.setText(hourlyData.get("Temp").toString() + "c");
+        hourlyFeelTemp.setText(hourlyData.get("TempFeel").toString() + "c");
+        hourlyHumidity.setText(hourlyData.get("Humidity").toString());
+        hourlyWindSpeed.setText(hourlyData.get("WindSpeed").toString());
+        hourlyPrecip.setText(hourlyData.get("Precip").toString());
+        timeCheck.setText(hourlyData.get("TimeStamp").toString());
+        artInt.setText(hourlyData.get("Summary").toString());
+        
+        switch (hourlyData.get("Icon").toString()) {
+            case "\"clear-day\"":
+                iconImg.setImage(CLEAR_DAY);
+                break;
+            case "\"clear-night\"":
+                iconImg.setImage(CLEAR_NIGHT);
+                break;
+            case "\"rain\"":
+                iconImg.setImage(RAIN);
+                break;
+            case "\"snow\"":
+                iconImg.setImage(SNOW);
+                break;
+            case "\"sleet\"":
+                iconImg.setImage(SLEET);
+                break;
+            case "\"wind\"":
+                iconImg.setImage(WIND);
+                break;
+            case "\"fog\"":
+                iconImg.setImage(FOG);
+                break;
+            case "\"cloudy\"":
+                iconImg.setImage(CLOUDY);
+                break;
+            case "\"partly-cloudy-day\"":
+                iconImg.setImage(PARTLY_CLOUDY_DAY);
+                break;
+            case "\"partly-cloudy-night\"":
+                iconImg.setImage(PARTLY_CLOUDY_NIGHT);
+                break;
+            default:
+                iconImg.setImage(UNKNOWN);
+                break;
+        }
+        
+    }
+    
+    private void setDailyData() {
+       // weatherAPI.dailyReport(1);
+        //System.out.println("IT FUCKING WORKS...:" + dailyData.get("Temp").toString());
+        //day1.setText(dailyData.get(""));
+        //day1Temp.setText(dailyData.get("Temp").toString() + "c");
+        //day1Temp.setText("Test");
+        /*
+       dailyData = weatherAPI.DailyReport(2);
+        //day2.setText("");
+     day2Temp.setText(dailyData.get("Temp").toString() + "c");
+        
+        dailyData = weatherAPI.DailyReport(3);
+        //day3.setText("");
+        day3Temp.setText(dailyData.get("Temp").toString() + "c");
+        
+        dailyData = weatherAPI.DailyReport(4);
+        //day4.setText("");
+        day4Temp.setText(dailyData.get("Temp").toString() + "c");
+        
+        dailyData = weatherAPI.DailyReport(5);
+        //day5.setText("");
+        day5Temp.setText(dailyData.get("Temp").toString() + "c");
+        
+        dailyData = weatherAPI.DailyReport(6);
+        //day6.setText("");
+        day6Temp.setText(dailyData.get("Temp").toString() + "c");*/
     }
 
 }
